890642fef60e60e3aa06566396b88e3aaf6a524d
b1z3rr4 <nataliasilva.bezerra18@gmail.com>
2022-06-11 14:08:03 -0300

    first commit

--- src/tasks.js
+++ src/tasks.js
@@ -1,31 +1,36 @@
 const colors = require("colors");
-/*
-Para ficar mais parecido com o exemplo do README.md do projeto, 
-use a libe *colors* para colorir as mensagens.
 
-As cores usadas no exemplo foram: red, magenta, cyan e green.
+const style = colors.setTheme({
+  tk1: "red",
+  tk2: "magenta",
+  tk3: "cyan",
+  tk4: "green"
+});
 
-Consulte a documentação da lib em https://github.com/Marak/colors.js
-*/
 
-// TODO 1: Essa Task deve retornar uma promise e com delay de 10 segundos
+const baseTask = (message, time) => {
+  time = time * 1000;
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve(message);
+    }, time);
+  });
+}
+
 const task1 = () => {
-  // Deve retornar a mensagem: Terminou a Task 1 em 10 segundos
+  return baseTask('Terminou a Task 1 em 10 segundos'.tk1, 10);
 };
 
-// TODO 2: Essa Task deve retornar uma promise e com delay de 5 segundos
 const task2 = () => {
-  // Deve retornar a mensagem: Terminou a Task 2 em 5 segundos
+  return baseTask('Terminou a Task 2 em 5 segundos'.tk2, 5);
 };
 
-// TODO 3: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task3 = () => {
-  // Deve retornar a mensagem: Terminou a Task 3 em 4 segundos
+  return baseTask('Terminou a Task 3 em 4 segundos'.tk3, 4);
 };
 
-// TODO 4: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task4 = () => {
-  // Deve retornar a mensagem: Terminou a Task 4 em 4 segundos
+  return baseTask('Terminou a Task 4 em 4 segundos'.tk4, 4);
 };
 
 module.exports = { task1, task2, task3, task4 };
