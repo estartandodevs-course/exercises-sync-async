d3813bbd7a3511856ee9c2e6fa8fdc9dda9a0b17
Kauan Lemos <kauanlemos@outlook.com.br>
2022-06-12 20:14:12 -0300

    working now just a lil more nice and easy

--- src/index.js
+++ src/index.js
@@ -1,44 +1,30 @@
 const { task1, task2, task3, task4 } = require("./tasks");
 
-const synchronousExecution = async () => {
+const makingAllPromise = async (tasks, time) => { //Fiz essa func pois no ultimo commit eu repetia as linhas nas 4 tasks, fiz bem ?
+  /**
+   * time = 1 = 1sec
+   */
   await new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(task1())}, 1000*10)    
-  });
-  await new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(task2())}, 1000*5)    
-  });
-  await new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(task3())}, 1000*4)    
-  });
-  await new Promise((resolve) => {
-    setTimeout(() => {
-        resolve(task4())}, 1000*4)    
-  });
-};
+      setTimeout(() => {
+        resolve(tasks())}, 1000*time) //*¹Tive que colocar a tasks com () isso é correto ?
+  })
+}
 
-const asynchronousExecution = () => {
-  setTimeout(task1, 1000*10);
+const synchronousExecution = async () => {
+  await makingAllPromise(task1, 10) //Bem mais simples ficou, acho que até facilitou na hora de ler !
+  await makingAllPromise(task2, 5)
+  await makingAllPromise(task3, 4)//*¹Se colocasse os () aqui, ele executava o console.log da task antes do timeout -obivious
+  await makingAllPromise(task4, 4)
+}
+
+const asynchronousExecution = () => { 
+  setTimeout(task1, 1000*10);// setTimeout já funcina como async, por isso só utilizei ela, isso é correto ?
   setTimeout(task2, 1000*5);
   setTimeout(task3, 1000*4);
-  setTimeout(task4, 1000*4);
+  setTimeout(task4, 1000*4); 
 };
 
 const arg = process.argv[2];
 
 (arg === 'sync')? synchronousExecution(): -1;
 (arg ==='async')? asynchronousExecution(): -1;
-/*
-    TODO 3: Agora esse arquivo deve ter a capacidade de receber parâmetros
-    de entrada. 
-    
-    Caso o parâmetro de entrada seja "sync" e chamará a função synchronousExecution()
-    Caso o parâmetro de entrada seja "async" e chamará a função asynchronousExecution()
-
-    Dica: O Node tem uma api nativa para tratar os parâmetros de entrada
-    consulte https://nodejs.org/docs/latest/api/process.html
-
-
-*/
