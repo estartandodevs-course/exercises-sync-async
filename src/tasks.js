e86b315942924e0154353cb7c4ae76173d5a255c
Karine <karinevitoria.marinho@gmail.com>
2022-06-18 11:43:10 -0300

    first task passed

--- src/tasks.js
+++ src/tasks.js
@@ -1,4 +1,6 @@
 const colors = require("colors");
+
+const oneSecond = 1000;
 /*
 Para ficar mais parecido com o exemplo do README.md do projeto, 
 use a libe *colors* para colorir as mensagens.
@@ -11,21 +13,42 @@ Consulte a documentação da lib em https://github.com/Marak/colors.js
 // TODO 1: Essa Task deve retornar uma promise e com delay de 10 segundos
 const task1 = () => {
   // Deve retornar a mensagem: Terminou a Task 1 em 10 segundos
+    let counter = new Promise (async (resolve, reject) => {
+    try {
+      console.time('task1');
+      setTimeout(() => {
+        console.timeEnd('task1');
+        resolve('Terminou a Task 1 em 10 segundos');
+      }, 10 * oneSecond);
+    } catch (err) {
+      reject (err);
+    }
+  })
+
+  return counter;
 };
+
+// task1().then((text) => {
+//    console.log(text)
+// })
+
 
 // TODO 2: Essa Task deve retornar uma promise e com delay de 5 segundos
 const task2 = () => {
   // Deve retornar a mensagem: Terminou a Task 2 em 5 segundos
+
 };
 
 // TODO 3: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task3 = () => {
   // Deve retornar a mensagem: Terminou a Task 3 em 4 segundos
+
 };
 
 // TODO 4: Essa Task deve retornar uma promise e com delay de 4 segundos
 const task4 = () => {
   // Deve retornar a mensagem: Terminou a Task 4 em 4 segundos
+
 };
 
 module.exports = { task1, task2, task3, task4 };
